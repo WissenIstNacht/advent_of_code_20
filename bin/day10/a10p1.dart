239fd30700f86b3deaa112b2f7f582d198f9114d
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-11 14:16:00 +0200

    refactored part 1

---
+++ bin/day10/a10p1.dart
@@ -0,0 +1,35 @@
+import '../../lib/solver.dart';
+
+class Day10Part1 extends Solver {
+  Day10Part1() : super(10);
+
+  @override
+  String solve(List<String> input) {
+    var jolts = input.map((s) => int.parse(s)).toList();
+    jolts.sort();
+    var diff1 = 0;
+    var diff3 = 0;
+
+    // source difference (from outlet to first adapter.)
+    switch (jolts.first) {
+      case 1:
+        diff1++;
+        break;
+      case 3:
+        diff3++;
+        break;
+      default:
+        break;
+    }
+
+    for (var i = 0; i < jolts.length - 1; i++) {
+      if (jolts[i] + 1 == jolts[i + 1]) diff1++;
+      if (jolts[i] + 3 == jolts[i + 1]) diff3++;
+    }
+
+    diff3++; // 3 jolts difference to device.
+    final res = diff1 * diff3;
+
+    return res.toString();
+  }
+}
