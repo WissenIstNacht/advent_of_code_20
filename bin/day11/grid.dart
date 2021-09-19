665aa595f356723c7f673cec6b7b47031641e550
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-19 22:57:44 +0200

    added grid utility methods

--- bin/day11/grid.dart
+++ bin/day11/grid.dart
@@ -1,19 +1,69 @@
 class Grid {
-  late List<List<int>> grid;
+  late int n, m;
+  late List<List<int>> matrix;
+  late List<List<int>> newMatrix;
 
   Grid.fromList(List<String> list) {
-    grid = list.map((row) {
+    matrix = list.map((row) {
       final intRow = <int>[];
       for (var i = 0; i < row.length; i++) {
-        if (intRow[i] == '.') {
+        if (row[i] == '.') {
           intRow.add(0);
         } else if (row[i] == '#') {
-          intRow.add(2);
-        } else {
           intRow.add(1);
+        } else {
+          intRow.add(2);
         }
       }
       return intRow;
     }).toList();
+    m = matrix.length;
+    n = matrix[0].length;
+    newMatrix = matrix;
   }
+
+  // given a position, return the surrounding elements
+  List<int> getSurroundingElements(x, y) {
+    var list = <int>[];
+    for (var i = x - 1; i <= x + 1; i++) {
+      for (var j = y - 1; j <= y + 1; j++) {
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+          continue;
+        }
+        list.add(matrix[i][j]);
+      }
+    }
+    return list;
+  }
+
+  bool isOccupied(x, y) {
+    return matrix[x][y] == 1;
+  }
+
+  bool isEmpty(x, y) {
+    return matrix[x][y] == 2;
+  }
+
+  bool hasOccupiedNeighbor(x, y) {
+    return getSurroundingElements(x, y).contains(1);
+  }
+
+  bool hasTooManyNeighbors(x, y) {
+    int folder(sum, element) => element == 1 ? sum + 1 : sum;
+    final occupiedNeighbors = getSurroundingElements(x, y).fold(0, folder);
+    return occupiedNeighbors >= 4;
+  }
+
+  // int newCellState(x, y) {
+  //   // if cell is empty and does not contain occupied neighbor
+  //   if (isEmpty(x, y) && !hasOccupiedNeighbor(x, y)) {
+  //     return 1;
+  //   }
+  //   // if cell is occupied  four or more seats are occupied
+  //   if (isOccupied(x, y) && ) {
+  //     return 2;
+  //   }
+  //   //return current state
+  //   return matrix[x][y];
+  // }
 }
