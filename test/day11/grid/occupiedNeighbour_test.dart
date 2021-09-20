2bfda8c5285b8c6cf7bcf64684ab2582a5bc822c
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-20 10:08:40 +0200

    renamed test files

--- test/day11/grid/occupiedNeighbour_test.dart
+++ test/day11/grid/occupiedNeighbour_test.dart
@@ -13,14 +13,19 @@ void main() {
     lines = File('test/day11/inputs/lsInput').readAsLinesSync();
     emptyGrid = Grid.fromList(lines);
   });
-  test('Check that basic inputs middle position has occupied neighbours', () {
+  test('Check that basic inputs middle has occupied neighbours', () {
     expect(basicGrid.hasOccupiedNeighbor(1, 1), equals(true));
   });
-  test('Check that dots input in middle position has no occupied neighbours',
-      () {
+  test('Check that basic inputs top right has no occupied neighbours', () {
+    expect(basicGrid.hasOccupiedNeighbor(0, 2), equals(false));
+  });
+  test('Check that basic inputs bottom left has occupied neighbours', () {
+    expect(basicGrid.hasOccupiedNeighbor(0, 2), equals(false));
+  });
+  test('Check that dots input in middle has no occupied neighbours', () {
     expect(floorGrid.hasOccupiedNeighbor(1, 1), equals(false));
   });
-  test('Check that ls inputs middle position hasno  occupied neighbours', () {
+  test('Check that ls inputs middle has no occupied neighbours', () {
     expect(emptyGrid.hasOccupiedNeighbor(1, 1), equals(false));
   });
 }
