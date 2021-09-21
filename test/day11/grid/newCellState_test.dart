5a1b780122610f76cda2d7c190cd6dd2018e11dd
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-21 22:04:08 +0200

    new cell state test

---
+++ test/day11/grid/newCellState_test.dart
@@ -0,0 +1,45 @@
+import 'package:test/test.dart';
+import 'dart:io';
+
+import '../../../bin/day11/grid.dart';
+
+void main() {
+  late Grid basicGrid, emptyGrid, fullGrid, diamondGrid;
+  setUp(() {
+    var lines = File('test/day11/inputs/basicInput').readAsLinesSync();
+    basicGrid = Grid.fromList(lines);
+    lines = File('test/day11/inputs/lsInput').readAsLinesSync();
+    emptyGrid = Grid.fromList(lines);
+    lines = File('test/day11/inputs/hashtagsInput').readAsLinesSync();
+    fullGrid = Grid.fromList(lines);
+    lines = File('test/day11/inputs/diamondInput').readAsLinesSync();
+    diamondGrid = Grid.fromList(lines);
+  });
+  test('Basic input middle does not change', () {
+    expect(basicGrid.newCellState(1, 1), equals(0));
+  });
+  test('Basic input bottom left does not change', () {
+    expect(basicGrid.newCellState(2, 0), equals(0));
+  });
+  test('Basic input top right does not change', () {
+    expect(basicGrid.newCellState(0, 2), equals(0));
+  });
+  test('Empty input middle input changes', () {
+    expect(emptyGrid.newCellState(1, 1), equals(1));
+  });
+  test('Empty input top left changes', () {
+    expect(emptyGrid.newCellState(0, 0), equals(1));
+  });
+  test('Full input top left does not change', () {
+    expect(fullGrid.newCellState(0, 0), equals(0));
+  });
+  test('Full input middle does change', () {
+    expect(fullGrid.newCellState(1, 1), equals(2));
+  });
+  test('Full input top middle does change', () {
+    expect(fullGrid.newCellState(0, 1), equals(2));
+  });
+  test('Diamond input top middle does change', () {
+    expect(fullGrid.newCellState(0, 1), equals(2));
+  });
+}
